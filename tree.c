d890a5e06574117be4dbd5bdd34ad3c032973e1a
Mehmet E Belviranli <54647757+belviranli@users.noreply.github.com>
2020-08-31 11:06:23 -0600

    Initial commit

---
+++ tree.c
@@ -0,0 +1,13 @@
+#include <errno.h>
+#include <dirent.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	printf("TODO: write a main function\n");
+	return 0;
+}
